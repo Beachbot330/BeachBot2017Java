14337023d6a3565e4ee4628606bc806b8ef4ea11
micro <micro@DESKTOP-SCRABCH.home>
2017-01-28 06:59:50 -0800

    Stashing changes

--- src/org/usfirst/frc333/commands/TankDrive.java
+++ src/org/usfirst/frc333/commands/TankDrive.java
@@ -37,10 +37,12 @@ public class TankDrive extends BBCommand {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.chassis.stopDrive();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.chassis.tankDrive(Robot.oi.getDriverL(), Robot.oi.getDriverR());
     }
 
     // Make this return true when this Command no longer needs to run execute()
