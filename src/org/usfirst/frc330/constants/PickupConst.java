039778799883f01ba4ad1e6cd66a702205ec519b
sciencewhiz <git@rossesmail.com>
2017-02-18 12:37:10 -0800

    Implement pickup methods

---
+++ src/org/usfirst/frc330/constants/PickupConst.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc330.constants;
+
+public class PickupConst {
+	
+	public static final double PICKUP_SPEED = 1.0;
+	public static final double PICKUP_REVERSE_SPEED = -1.0;
+
+}
