47d47b1a652025132ba4e3484a40d9bf383d5693
sciencewhiz <git@rossesmail.com>
2017-03-18 18:44:59 -0700

    Initial version of aiming from vision

---
+++ src/org/usfirst/frc330/commands/drivecommands/TurnCamera.java
@@ -0,0 +1,83 @@
+package org.usfirst.frc330.commands.drivecommands;
+
+import org.usfirst.frc330.Robot;
+import org.usfirst.frc330.util.Logger;
+import org.usfirst.frc330.util.Logger.Severity;
+import org.usfirst.frc330.wpilibj.PIDGains;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TurnCamera extends TurnGyroRel {
+	String cameraName;
+	int toleranceStableCount;
+	
+	
+	/**
+	 * Turn the base until it aligns with the vision target.
+	 * 
+	 * @param cameraName name of the camera Distance/Detected (airship or target)
+	 * @param tolerance degrees
+	 * @param toleranceStableCount number of iterations to be within tolerance before finishing. Should be 10 or more to account for camera latency.
+	 * @param timeout seconds
+	 * @param stopAtEnd
+	 * @param gains
+	 */
+	public TurnCamera(String cameraName, double tolerance, int toleranceStableCount, double timeout, boolean stopAtEnd, PIDGains gains) {
+		super(0, tolerance, timeout, stopAtEnd, gains);
+		this.cameraName = cameraName;
+		this.toleranceStableCount = toleranceStableCount;
+	}
+
+	/**
+	 * Turn the base until it aligns with the vision target.
+	 * 
+	 * @param cameraName name of the camera Distance/Detected (airship or target)
+	 * @param tolerance degrees
+	 * @param toleranceStableCount number of iterations to be within tolerance before finishing. Should be 10 or more to account for camera latency.
+	 * @param gains
+	 */
+	public TurnCamera(String cameraName, double tolerance, int toleranceStableCount, PIDGains gains) {
+		super(0, tolerance, gains);
+		this.cameraName = cameraName;
+		this.toleranceStableCount = toleranceStableCount;
+	}
+	
+	@Override
+	public void initialize() {
+		if (!SmartDashboard.containsKey(cameraName+"Detected"))
+			Logger.getInstance().println("Camera Name: " + cameraName + " Not detected", Severity.ERROR);
+		angle = getCameraAngle();
+		super.initialize();
+	}
+	
+	@Override
+	public void execute() {
+		if (Math.abs(getCameraAngle() - angle) > 1.0) {
+			Robot.chassis.gyroPID.setSetpoint(getCameraAngle()+Robot.chassis.getAngle());
+		}
+			
+		super.execute();
+	}
+	
+	int toleranceCount = 0;
+	
+	@Override
+	public boolean isFinished() {
+		if (super.isFinished())
+			toleranceCount++;
+		else
+			toleranceCount = 0;
+		return getCameraDetected() && toleranceCount > 13;   //
+	}
+	
+	public double getCameraAngle() {
+		if (getCameraDetected())
+			return SmartDashboard.getNumber(cameraName+"Angle", 0);
+		else
+			return 0;
+	}
+	
+	public boolean getCameraDetected() {
+		return SmartDashboard.getBoolean(cameraName+"Detected", false);
+	}
+}
